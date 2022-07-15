f1f97de82294c80a2797c00d8b631fde83829a68
Maxim <m.krishnevskiy@kmg.kz>
2022-07-15 18:26:37 +0600

    DZ

---
+++ DZ50/Program.cs
@@ -0,0 +1,72 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+
+int[,] CreateMatr(int n, int m)
+{
+    int[,] matrix = new int[n, m];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 100);
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatr(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+        Console.Write("введите число: c = ");
+        int c = Convert.ToInt32(Console.ReadLine());
+            if (c == CreateMatr[i,j])
+                 {
+                     Console.Write("Такое число есть");
+
+                }
+        Console.Write("Такого  числа нет ");
+    }
+}
+    
+
+PrintMatr(CreateMatr(3, 4));
+
+
+
+// Ручной ввод
+// int[, ] CreateMatr(int n, int m)
+// {
+//     int[, ] matrix = new int[n ,m];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+//         }
+//     }
+//     return matrix;
+// }
+
+
+// void PrintMatr(int[, ] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write(matrix[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int n = Convert.ToInt32(Console.ReadLine());
+// int m = Convert.ToInt32(Console.ReadLine());
+// PrintMatr(CreateMatr(n, m));
---
+++ DZ52/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+
+int[, ] CreateMatr(int n, int m)
+{
+    int[, ] matrix = new int[n ,m];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 100);
+            //matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return matrix;
+}
+
+
+void PrintMatr(int[, ] matrix)
+{
+    Console.WriteLine();
+    int AverageSum = 0, count = 1;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    { 
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            
+             AverageSum = (AverageSum + matrix[i, j])/count;
+             count++;
+            Console.Write(Convert.ToDouble(AverageSum));
+    }
+    //Console.Write(Convert.ToDouble(AverageSum  + " "));
+}
+
+Console.Write("Введите колличество строк n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите колличество столбцоы  m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+PrintMatr(CreateMatr(n, m));
